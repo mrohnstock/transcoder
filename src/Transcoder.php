99768f8ebefde48faac46239b77965a903320534
Mathias Rohnstock <m.rohnstock@googlemail.com>
2017-06-26 09:34:45 +0200

    use mb_detect_encoding to determine input charset, if available

--- src/Transcoder.php
+++ src/Transcoder.php
@@ -24,8 +24,10 @@ class Transcoder implements TranscoderInterface
      */
     public function transcode($string, $from = null, $to = null)
     {
-        if ($from == "unicode-1-1-utf-7")
-            $from = "UTF-7";
+        if (function_exists('mb_detect_encoding')) {
+            $_from = mb_detect_encoding($string, "UTF-8, UTF-7, ASCII, ISO-8859-1, ISO-8859-2, ISO-8859-3, ISO-8859-4, ISO-8859-5, ISO-8859-6, ISO-8859-7, ISO-8859-8, ISO-8859-9, ISO-8859-10, ISO-8859-13, ISO-8859-14, ISO-8859-15, ISO-8859-16, Windows-1251, Windows-1252, Windows-1254");
+            $from = ($_from != "" ? $_from : $from);
+        }
 
         foreach ($this->transcoders as $transcoder) {
             try {
